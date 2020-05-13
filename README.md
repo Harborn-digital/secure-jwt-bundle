fbba1b3a20418c3d3ea7e762f11c6189a9839c0e
Ron Rademaker <ron@connectholland.nl>
2020-05-13 11:37:27 +0200

    Add docs

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # secure-jwt
 Library that makes JWT more secure
+
+## Cookie storage
+Tokens in local storage are insecure, so if you use tokens from a web interface you should store them somewhere else. A secure cookie is a good location. Configure cookie storage as follows:
+
+### Let the lexik/jwt-authentication-bundle look at cookies:
+
+In the `lexik_jwt_authentication.yaml` config file:
+```yaml
+lexik_jwt_authentication:
+    secret_key: '%env(resolve:JWT_SECRET_KEY)%'
+    public_key: '%env(resolve:JWT_PUBLIC_KEY)%'
+    pass_phrase: '%env(JWT_PASSPHRASE)%'
+
+    token_extractors:
+            # Default header auth, can be useful to allow for other auth types (for example /api)
+            authorization_header:
+                enabled: true
+
+            # Make sure this is enabled
+            cookie:
+                enabled: true
+                name:    BEARER
+```
+
+### Make sure the token is set as a secure cookie
+
+In the `security.yaml` config file:
+```yaml
+    login:
+        pattern:  ^/api/login
+        stateless: true
+        anonymous: true
+        json_login:
+            check_path:               /api/login_check
+            success_handler:          ConnectHolland\SecureJWT\Security\Http\Authentication\AuthenticationSuccessHandler
+            failure_handler:          lexik_jwt_authentication.handler.authentication_failure
+```
---
+++ tests/Security/Http/Authentication/AuthenticationSuccessHandlerTest.php
@@ -0,0 +1,156 @@
+<?php
+
+/*
+ * This file is part of the Connect Holland Secure JWT package.
+ * (c) Connect Holland.
+ */
+
+namespace ConnectHolland\SecureJWT\Tests\Security\Http\Authentication;
+
+use ConnectHolland\SecureJWT\Security\Http\Authentication\AuthenticationSuccessHandler;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Events;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTManager;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Variant of Lexik\Bundle\JWTAuthenticationBundle\Security\Http\Authentication\AuthenticationSuccessHandler.
+ */
+class AuthenticationSuccessHandlerTest extends TestCase
+{
+    /**
+     * test onAuthenticationSuccess method.
+     */
+    public function testOnAuthenticationSuccess(): void
+    {
+        $request = $this->getRequest();
+        $token   = $this->getToken();
+
+        $response = (new AuthenticationSuccessHandler($this->getJWTManager('secrettoken'), $this->getDispatcher()))
+            ->onAuthenticationSuccess($request, $token);
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $content = json_decode($response->getContent(), true);
+        $cookies = $response->headers->getCookies();
+        $this->assertArrayNotHasKey('token', $content);
+        $this->assertArrayHasKey('result', $content);
+        $this->assertCount(1, $cookies);
+        $this->assertSame('secrettoken', $cookies[0]->getValue());
+    }
+
+    public function testHandleAuthenticationSuccess()
+    {
+        $response = (new AuthenticationSuccessHandler($this->getJWTManager('secrettoken'), $this->getDispatcher()))
+            ->handleAuthenticationSuccess($this->getUser());
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $content = json_decode($response->getContent(), true);
+        $cookies = $response->headers->getCookies();
+
+        $this->assertArrayNotHasKey('token', $content);
+        $this->assertArrayHasKey('result', $content);
+        $this->assertCount(1, $cookies);
+        $this->assertSame('secrettoken', $cookies[0]->getValue());
+    }
+
+    public function testHandleAuthenticationSuccessWithGivenJWT()
+    {
+        $response = (new AuthenticationSuccessHandler($this->getJWTManager(), $this->getDispatcher()))
+            ->handleAuthenticationSuccess($this->getUser(), 'jwt');
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $content = json_decode($response->getContent(), true);
+        $cookies = $response->headers->getCookies();
+
+        $this->assertArrayNotHasKey('token', $content);
+        $this->assertArrayHasKey('result', $content);
+        $this->assertCount(1, $cookies);
+        $this->assertSame('jwt', $cookies[0]->getValue());
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getRequest()
+    {
+        $request = $this
+            ->getMockBuilder('Symfony\Component\HttpFoundation\Request')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        return $request;
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getToken()
+    {
+        $token = $this
+            ->getMockBuilder('Lexik\Bundle\JWTAuthenticationBundle\Security\Authentication\Token\JWTUserToken')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $token
+            ->expects($this->any())
+            ->method('getUser')
+            ->will($this->returnValue($this->getUser()));
+
+        return $token;
+    }
+
+    private function getUser()
+    {
+        $user = $this
+            ->getMockBuilder('Symfony\Component\Security\Core\User\UserInterface')
+            ->getMock();
+
+        $user
+            ->expects($this->any())
+            ->method('getUsername')
+            ->will($this->returnValue('username'));
+
+        return $user;
+    }
+
+    private function getJWTManager($token = null)
+    {
+        $jwtManager = $this->getMockBuilder(JWTManager::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        if (null !== $token) {
+            $jwtManager
+                ->expects($this->any())
+                ->method('create')
+                ->will($this->returnValue('secrettoken'));
+        }
+
+        return $jwtManager;
+    }
+
+    private function getDispatcher()
+    {
+        $dispatcher = $this->getMockBuilder(EventDispatcher::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $dispatcher
+            ->expects($this->once())
+            ->method('dispatch')
+            ->with(
+                $this->isInstanceOf(AuthenticationSuccessEvent::class),
+                $this->equalTo(Events::AUTHENTICATION_SUCCESS)
+            );
+
+        return $dispatcher;
+    }
+}
