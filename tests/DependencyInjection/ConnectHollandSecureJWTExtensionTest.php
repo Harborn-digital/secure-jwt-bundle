6d32a7b26cc8c940ce245e9880cc7b2a7413f0d8
Ron Rademaker <ron@connectholland.nl>
2020-05-14 13:30:53 +0200

    Add test for extension

---
+++ tests/DependencyInjection/ConnectHollandSecureJWTExtensionTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the Connect Holland Secure JWT package.
+ * (c) Connect Holland.
+ */
+
+namespace ConnectHolland\SecureJWTBundle\Tests\DependencyInjection;
+
+use ConnectHolland\SecureJWTBundle\DependencyInjection\ConnectHollandSecureJWTExtension;
+use ConnectHolland\SecureJWTBundle\EventSubscriber\LoginSubscriber;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class ConnectHollandSecureJWTExtensionTest extends TestCase
+{
+    public function testLoad(): void
+    {
+        $container = new ContainerBuilder();
+        $extension = new ConnectHollandSecureJWTExtension();
+        $extension->load([], $container);
+
+        $this->assertTrue($container->has('connectholland.secure_jwt.two_factor_jwt.listener'), 'Security listener should be loaded');
+        $this->assertTrue($container->has(LoginSubscriber::class), 'Auto configuration should load LoginSubscriber');
+    }
+}
