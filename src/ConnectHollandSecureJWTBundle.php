a19fb52c8a42791a061dfd85affe6267b1a04b6c
Ron Rademaker <ron@connectholland.nl>
2020-05-14 14:07:27 +0200

    Register security factory

--- src/ConnectHollandSecureJWTBundle.php
+++ src/ConnectHollandSecureJWTBundle.php
@@ -7,8 +7,17 @@
 
 namespace ConnectHolland\SecureJWTBundle;
 
+use ConnectHolland\SecureJWTBundle\Security\Factory\TwoFactoryJWTFactory;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ConnectHollandSecureJWTBundle extends Bundle
 {
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $extension = $container->getExtension('security');
+        $extension->addSecurityListenerFactory(new TwoFactoryJWTFactory());
+    }
 }
