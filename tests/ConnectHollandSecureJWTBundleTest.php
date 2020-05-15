02216b3639860839201534c766c304abc801b9a2
Ron Rademaker <ron@connectholland.nl>
2020-05-15 11:05:19 +0200

    Add test for bundle class

---
+++ tests/ConnectHollandSecureJWTBundleTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the Connect Holland Secure JWT package.
+ * (c) Connect Holland.
+ */
+
+namespace ConnectHolland\SecureJWTBundle\Tests;
+
+use ConnectHolland\SecureJWTBundle\ConnectHollandSecureJWTBundle;
+use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\SecurityBundle\DependencyInjection\SecurityExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class ConnectHollandSecureJWTBundleTest extends TestCase
+{
+    public function testSecurityFactoryIsAdded(): void
+    {
+        $bundle    = new ConnectHollandSecureJWTBundle();
+        $container = $this->createMock(ContainerBuilder::class);
+        $extension = $this->createMock(SecurityExtension::class);
+
+        $container
+            ->expects($this->once())
+            ->method('getExtension')
+            ->willReturn($extension);
+
+        $extension
+            ->expects($this->once())
+            ->method('addSecurityListenerFactory');
+
+        $bundle->build($container);
+    }
+}
