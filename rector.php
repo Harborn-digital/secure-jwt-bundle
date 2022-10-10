2096b46490313d63f175850bc594a012ffe1e387
Luc1405 <1017799@hr.nl>
2022-10-10 11:56:17 +0200

    Removed some unused imports

--- rector.php
+++ rector.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\LevelSetList;
 use Rector\Symfony\Set\SymfonySetList;
-use Rector\Doctrine\Set\DoctrineSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
@@ -19,6 +17,5 @@ return static function (RectorConfig $rectorConfig): void {
         SymfonySetList::SYMFONY_54,
         SymfonySetList::SYMFONY_CODE_QUALITY,
         SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
-        DoctrineSetList
     ]);
 };
--- src/Security/Guard/JWTTokenAuthenticator.php
+++ src/Security/Guard/JWTTokenAuthenticator.php
@@ -22,8 +22,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Contracts\Translation\TranslatorInterface;
-
 
 class JWTTokenAuthenticator extends BaseAuthenticator
 {
